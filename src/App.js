40e08ebb14c0835784c56c2f01efc183a86fd1e0
Matteo Manzinello <matteo@manzinello.com>
2018-04-09 11:49:22 +0200

    Aggiunto Grommet, aggiustamenti vari HTML e React

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { InstantSearch, SearchBox, Hits, Menu } from 'react-instantsearch/dom';
+import { InstantSearch, SearchBox, Hits, Pagination } from 'react-instantsearch/dom';
+import Card from 'grommet/components/Card';
+
+function Ufficio({ hit }) {
+  return <div>{hit._nomeufficio}</div>;
+}
 
 const App = () => (
-  <InstantSearch
-    appId="2CVZTW5VOB"
-    apiKey="a053bf374d1bac46fd8b15c47cb607c3"
-    indexName="uffici"
-  >
-    <SearchBox />
-    <Hits />
-  </InstantSearch>
+  <div>
+    <InstantSearch
+      appId="2CVZTW5VOB"
+      apiKey="a053bf374d1bac46fd8b15c47cb607c3"
+      indexName="uffici"
+    >
+      <SearchBox
+        style={{ marginBottom: "20px" }}
+      />
+      <Hits hitComponent={Ufficio}
+      />
+      <Pagination />
+    </InstantSearch>
+  </div>
 );
 
 export default App;
